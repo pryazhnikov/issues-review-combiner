a227477b0949801b0b4ef5701f92df2ec7352ba2
Marcus Artner <git@martner.at>
2021-10-21 10:20:00 +0200

    Implements issue codes extraction (#10)
    Refs: https://github.com/pryazhnikov/issues-review-combiner/issues/3

--- src/IssueDetector.php
+++ src/IssueDetector.php
@@ -18,6 +18,16 @@ class IssueDetector
         // Lines to match: "SRV-123" / "* SRV-123" / " -  SRV-123"
         if (preg_match('#^\s*([A-Z]+-\d+)\s*#i', $cleanLine, $match)) {
             return $match[1];
+        } else if(preg_match('#\.atlassian\.net/jira[\/\w\d?=&]+&selectedIssue=([A-Z]+-\d+)#i', $cleanLine, $match)) {
+            return $match[1];
+        } else if(preg_match('#\.atlassian\.net/browse/([A-Z]+-\d+)#i', $cleanLine, $match)) {
+            return $match[1];
+        } else if(preg_match('#youtrack\.jetbrains\.com/issue/([A-Z]+-\d+)#i', $cleanLine, $match)) {
+            return $match[1];
+        } else if(preg_match('#redmine\.org/issues/(\d+)#i', $cleanLine, $match)) {
+            return $match[1];
+        } else if(preg_match('#github\.com/[\w_-]+/[\w_-]+/issues/(\d+)#i', $cleanLine, $match)) {
+            return $match[1];
         } else {
             return null;
         }
--- tests/IssueDetectorTest.php
+++ tests/IssueDetectorTest.php
@@ -14,8 +14,16 @@ class IssueDetectorTest extends TestCase
             'Empty case #1' => ['', null],
             'Empty case #2' => ["\t\t", null],
             'Empty case #3' => ['* (Other) Restarting data filling in the service', null],
-            ["* (On production) SRVLAB-1234 Code change notifications rollout", 'SRVLAB-1234'],
-            [' - SRV-12321 (Code review) Hidden staffpass login should not automatically', 'SRV-12321'],
+            'Issue case #1' => ["* (On production) SRVLAB-1234 Code change notifications rollout", 'SRVLAB-1234'],
+            'Issue case #2' => [' - SRV-12321 (Code review) Hidden staffpass login should not automatically', 'SRV-12321'],
+            'Jira-URL case #1: long format line-end' => ['see https://test.atlassian.net/jira/software/c/projects/TE/boards/1?modal=detail&selectedIssue=TE-123', 'TE-123'],
+            'Jira-URL case #2: long format more text' => ['see https://corporation.atlassian.net/jira/software/c/projects/UDX/boards/1?modal=detail&selectedIssue=UDX-145 for more details', 'UDX-145'],
+            'Jira-URL case #3: short format line-end' => ['Refs: https://something.atlassian.net/browse/XY-456','XY-456'],
+            'Jira-URL case #3: short format more text' => ['Refs: https://acme.atlassian.net/browse/ABC-6575 and other issues','ABC-6575'],
+            'Youtrack-URL case #1' => ['https://youtrack.jetbrains.com/issue/KT-12345', 'KT-12345'],
+            'RedMine-URL case #1' => ['https://www.redmine.org/issues/33333', '33333'],
+            'GitHub-URL case #1' => ['https://github.com/pryazhnikov/issues-review-combiner/issues/123', '123'],
+            'GitHub-URL case #2' => ['https://github.com/someuser/important_project/issues/456', '456'],
         ];
     }
 
